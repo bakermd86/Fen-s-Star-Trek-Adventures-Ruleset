f9e9e830791d0fb5e3000c2cf483e144c8172adc
bakermd86 <bakermd86@gmail.com>
2022-01-27 20:56:49 -0500

    Changing the ruleset to CoreRPG (I forgot that this was set to this other ruleset...)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -f Fen_STA.ext;
 zip Fen_STA.ext -x build.sh -r ./*;
 mv Fen_STA.ext /mnt/c/Users/Michael/AppData/Roaming/SmiteWorks/Fantasy\ Grounds/extensions/
-rm -f /mnt/c/Users/Michael/AppData/Roaming/SmiteWorks/Fantasy\ Grounds/cache/test7/extensions/*
+rm -f /mnt/c/Users/Michael/AppData/Roaming/SmiteWorks/Fantasy\ Grounds/cache/test8/extensions/*
